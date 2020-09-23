2253ef4ee6ad7c5a34c12d501ee9460c09cc8440
Jabez <davidjthw@gmail.com>
2020-09-24 00:53:12 +0800

    1) house keeping && register API 2) consume API && Frontend 3) auth API, userProfile API

---
+++ backend/models/user.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'An email is required(server)'
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty(server)',
+        minlength: [7, 'Password must be atleast 7 characters long']
+    },
+    saltSecret : String
+});
+
+// Custom Calidation for email
+userSchema.path('email').validate((val)=>{
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail');
+
+// Events
+userSchema.pre('save', function(next){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+
+// Methods
+userSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+
+userSchema.methods.generateJwt = function (){
+        return jwt.sign({ _id: this._id },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXP }
+
+        );
+}
+
+mongoose.model('User', userSchema);
